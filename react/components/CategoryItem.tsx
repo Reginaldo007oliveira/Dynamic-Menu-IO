e5767e928d5d8aaf7fcf4c5b0c18d610978ef5a6
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-13 13:00:58 -0300

    Deu Ruim

--- react/components/CategoryItem.tsx
+++ react/components/CategoryItem.tsx
@@ -3,7 +3,16 @@ import React, { Fragment } from 'react'
 const CategoryItem = ({ id, name, url, title }: Category) => {
   return (
     <Fragment>
-      <div className="display-flex  flex-row" key={id}>
+      <div
+        className="display: flex;
+    justify-content: center;
+    /* align-items: center; */
+    justify-content: space-around;
+    text-decoration: none;
+    text-decoration-style: none;
+    text-shadow: 0 0 black;"
+        key={id}
+      >
         <a title={title} href={url}>
           <p>{name}</p>
         </a>
---
+++ react/components/categoryItem.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const ServicesContainer = styled.section`
+display: flex;
+justify-content: center;
+/* align-items: center; */
+justify-content: space-around;
+text-decoration: none;
+text-decoration-style: none;
+text-shadow: 0 0 black;
+  }
+`
